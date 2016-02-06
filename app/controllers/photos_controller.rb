251706c61542c6ea91c3c93c3a98303d9565a89a
Brian Rainey <raineyb@gmail.com>
2016-02-06 15:03:07 -0600

    Up to section 5.8 of tutorial.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,8 +1,21 @@
 class PhotosController < ApplicationController
-  def new
 
+  def show
+    @photo = Photo.find(params[:id])
   end
+
+  def new
+  end
+
   def create
-    render plain: params[:photo].inspect
+    @photo = Photo.new(photo_params)
+    @photo.save
+    redirect_to @photo
   end
+
+  private
+    def photo_params
+      params.require(:photo).permit(:path)
+    end
+
 end
