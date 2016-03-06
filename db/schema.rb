3e1aa7366dbf19204e5cd54de94b9394b28ae326
Brian Rainey <raineyb@gmail.com>
2016-03-06 11:47:04 -0600

    Add some users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160213012042) do
+ActiveRecord::Schema.define(version: 20160306172154) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "commenter"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20160213012042) do
     t.string   "path"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "user_name"
+    t.string   "display_name"
+    t.string   "relationship"
+    t.string   "password"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
