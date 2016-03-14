e68d0dffe7f3002de0cfcdb145fd09e6b942fcf3
Brian Rainey <raineyb@gmail.com>
2016-03-13 19:13:07 -0500

    Small tweaks to UI and config.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -8,19 +8,6 @@ class PhotosController < ApplicationController
     @photo = Photo.find(params[:id])
   end
 
-  def new
-    @photo = Photo.new
-  end
-
-  def create
-    @photo = Photo.new(photo_params)
-    if @photo.save
-      redirect_to @photo
-    else
-      render 'new'
-    end
-  end
-
   def img
     photo = Photo.find(params[:id])
     send_file("#{Rails.root}/photos/#{photo.path}",
